dd4bf78461707675c1b7b85ffe7497405ecb6f08
钱坤 <153087222@qq.com>
2019-11-21 22:34:55 +0800

    可以在浏览器显示

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry:{
-        index:'./lib/index.tsx'
+    mode:'production',
+    entry: {
+        index: './lib/index.tsx'
     },
-    module:{
-        rules:{
-            test:/\.tsx?$/,
-            loader:'awesome-typescript-loade'
-        }
-    }
-}
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        library: 'Wron',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'Wron',
+            template:"index.html"
+        })
+    ]
+};
