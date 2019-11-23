2a4d68faa479401093e55247c11ef8540774e58a
钱坤 <153087222@qq.com>
2019-11-23 15:04:54 +0800

    添加webpack的externals配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    mode:'production',
+    mode: 'development',
     entry: {
         index: './lib/index.tsx'
     },
-    resolve:{
-        extensions:['.ts','.tsx','.js','.jsx']
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
     },
     output: {
         path: path.resolve(__dirname, 'dist/lib'),
@@ -21,10 +21,24 @@ module.exports = {
             }
         ]
     },
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
-            title:'Wron',
-            template:"index.html"
+            title: 'Wron',
+            template: "index.html"
         })
-    ]
+    ],
+    externals: {
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+            root: 'React'
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDom'
+        }
+    }
 };
