e6aefb77a3bf357d6ac1837477b0a00f2a57ece7
钱坤 <153087222@qq.com>
2019-11-24 22:42:38 +0800

    创建icon组件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,8 @@
-import Button from './button';
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import Icon from './icon';
+
+ReactDom.render(<Icon name='wechat'/>,document.body);
 
 
-export {Button}
