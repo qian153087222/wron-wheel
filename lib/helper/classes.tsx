f94a67febe2aac0f9dfa9150254670b2a8df841d
钱坤 <153087222@qq.com>
2019-11-27 20:40:06 +0800

    优化className合并工具classes

--- lib/helper/classes.tsx
+++ lib/helper/classes.tsx
@@ -1,7 +1,5 @@
 //className合并工具
-const classes = (...name:(string | undefined)[]):string => {
-    //Boolean传入为Boolean，那么久只返回正确的
-    return name.filter(Boolean).join(' ');
-};
-
+//filter传入为Boolean，那么就只返回正确的
+const classes = (...name:(string | undefined)[]):string => name.filter(Boolean).join(' ');
+    
 export default classes;
