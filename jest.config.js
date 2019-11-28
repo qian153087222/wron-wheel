03924250ad68b31d8d4653a82796b89f943a0f24
钱坤 <153087222@qq.com>
2019-11-28 22:26:52 +0800

    自动化初步配置

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     verbose:true,
     clearMocks:false,
     collectCoverage:false,
-    reporters:['default'],
+    reporters:['default','jest-junit'],
     moduleFileExtensions:['js','jsx','ts','tsx'],
     moduleDirectories:['node_modules'],
     globals:{
