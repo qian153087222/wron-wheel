bc5de7fc3cd8b6ca405079668b07936873648f81
钱坤 <153087222@qq.com>
2019-12-03 22:23:49 +0800

    auto publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,12 +1,2 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
-import Icon from './icon/icon';
-
-ReactDom.render(
-    <div>
-        <Icon name='qq' />
-    </div>
-    , document.querySelector('#root'));
-
+export { default as Icon } from './icon/icon';
 
