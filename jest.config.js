3b634fdadc582c6a58c27c71c808dba0c6746b99
钱坤 <153087222@qq.com>
2019-12-04 22:38:53 +0800

    add ci config

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,8 @@ module.exports = {
     verbose:true,
     clearMocks:false,
     //配置circle CI 持续化集成
-    collectCoverage:true,//是否收集测试覆盖率
-    collectCoverageFrom:["{lib,include}/**/*.{ts,tsx}","!**/node_modules/**"],
-    //测试目录，但不会收集__xx__目录，任意node_modules都不测试
-    coverageDirectory:'coverage',//生成报告放在哪
-    coverageReporters:['text','lcov'],//有哪些报告，text控制台输出，lcov著名的分析报告
-    reporters: [ "default", "jest-junit" ],
+    collectCoverage:false,//是否收集测试覆盖率
+    reporters: [ "default"],
     moduleFileExtensions:['js','jsx','ts','tsx'],
     moduleDirectories:['node_modules'],
     globals:{
